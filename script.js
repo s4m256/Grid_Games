d1fa106e2ba1a862c755a38877ef5e58bee86b09
samuel-santiago <al.2312.samuelsantiago@gmail.com>
2025-12-30 15:44:17 -0300

    add colorable cells with palette selection

--- script.js
+++ script.js
@@ -6,12 +6,41 @@ btn.onclick = () => {
     const n = Number(document.getElementById("n").value);
 
     board.innerHTML = "";
-    board.style.gridTemplateRows = `repeat(${m}, 50px)`;
-    board.style.gridTemplateColumns = `repeat(${n}, 50px)`;
+    board.style.gridTemplateRows = `repeat(${m}, 40px)`;
+    board.style.gridTemplateColumns = `repeat(${n}, 40px)`;
+
+    boardState = Array(m).fill(null).map(()=>Array(n).fill(null));
 
     for(let i = 0; i < m * n; i++) {
         const cell = document.createElement("div");
         cell.className = "cell";
         board.appendChild(cell);
+
+        cell.onclick = () => {
+            const currentColor = cell.dataset.color || "";
+
+            if(currentColor == selectedColor) {
+                cell.style.backgroundColor = "";
+                cell.dataset.color = "";
+                cell.classList.remove("marked");
+            } else {
+                cell.style.backgroundColor = selectedColor;
+                cell.dataset.color = selectedColor;
+                cell.classList.add("marked");
+            }
+        };
     }
 };
+
+
+let selectedColor = "black";
+
+document.querySelectorAll(".color").forEach(btn => {
+    btn.onclick = () => {
+        document.querySelectorAll(".color")
+            .forEach(b => b.classList.remove("selected"));
+        
+        btn.classList.add("selected");
+        selectedColor = btn.dataset.color;
+    };
+});
